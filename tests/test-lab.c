5772b714298705e0e0256a2229484f12a7ae0a1d
Christobal <christobalserra@u.boisestate.edu>
2025-01-29 19:12:06 -0700

    Expanded tests and bug fix.

--- tests/test-lab.c
+++ tests/test-lab.c
@@ -6,7 +6,7 @@ List* list;
 
 Node* add_data(List* list, int data) {
     list_add_node(list, &data);
-    return list->sentinel->previous; // Return new node (which is always the one before sentinel)
+    return list->sentinel->prev; // Return new node (which is always the one before sentinel)
 }
 
 void setUp(void) {
@@ -72,7 +72,17 @@ void test_remove2(void) {
     TEST_ASSERT_EQUAL_PTR(list->sentinel->next, second_node);  // Second node is now the first node
     TEST_ASSERT_EQUAL_PTR(second_node->next, list->sentinel);  // Second node points to sentinel
 
-    // TODO - add again and remove the second one this time.
+    // Add a node back and remove the tail one (third, since we add to tail) this time.
+    int data3 = 30;
+    Node* third_node = add_data(list, &data3);
+    Node* removed_node = list_remove(list, third_node);
+    TEST_ASSERT_EQUAL_PTR(removed_node, third_node);
+    TEST_ASSERT_EQUAL_PTR(list->sentinel->prev, second_node);  // Second node is now the tail node
+    TEST_ASSERT_EQUAL_PTR(second_node->next, list->sentinel);  // Second node points to sentinel
+
+    // Remove last node.
+    Node* removed_node = list_remove(list, second_node);
+    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, list->sentinel);  // list is empty again
 }
 
 void test_remove3(void) {
@@ -90,8 +100,25 @@ void test_remove3(void) {
     TEST_ASSERT_EQUAL_PTR(second_node->next, third_node);  // Second node points to third node
     TEST_ASSERT_EQUAL_PTR(third_node->next, list->sentinel);  // Third node points to sentinel
 
-    // TODO - add back and remove middle
-    // TODO - add back and remove third
+    // Add a node back and remove middle one this time.
+    int data4 = 14;
+    Node* fourth_node = add_data(list, &data4);
+    TEST_ASSERT_EQUAL_PTR(removed_node, third_node);
+    TEST_ASSERT_EQUAL_PTR(second_node->next, fourth_node);  // Second node points to fourth node
+    TEST_ASSERT_EQUAL_PTR(fourth_node->prev, second_node);  // Fourth node points to second
+
+    // Add a node back and remove tail one this time.
+    int data5 = 16;
+    Node* fifth_node = add_data(list, &data5);
+    TEST_ASSERT_EQUAL_PTR(removed_node, fifth_node);
+    TEST_ASSERT_EQUAL_PTR(list->sentinel->prev, fourth_node);  // Tail is now fourth node
+    TEST_ASSERT_EQUAL_PTR(list->sentinel, fourth_node->next); // Fourth points to sentinel
+
+    // Remove last nodes.
+    Node* removed_node = list_remove(list, second_node);
+    Node* removed_node = list_remove(list, fourth_node);
+    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, list->sentinel);  // list is empty again
+
 }
 
 void test_list_destroy(void) {
