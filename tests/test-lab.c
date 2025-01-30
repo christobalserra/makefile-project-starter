92f5f6afccf8649f897a9e696e1a7ed1ae81b5fd
Christobal <christobalserra@u.boisestate.edu>
2025-01-29 19:20:05 -0700

    Kind of depressed to realize there was an existing test suite NOW. :(

--- tests/test-lab.c
+++ tests/test-lab.c
@@ -1,139 +1,259 @@
-
 #include "harness/unity.h"
 #include "../src/lab.h"
 
-List* list;
 
-Node* add_data(List* list, int data) {
-    list_add_node(list, &data);
-    return list->sentinel->prev; // Return new node (which is always the one before sentinel)
+static list_t *lst_ = NULL;
+
+static int *alloc_data(int i)
+{
+  int *rval = (int *)malloc(sizeof(int));
+  *rval = i;
+  return rval;
 }
 
+static void destroy_data(void *data)
+{
+  free(data);
+}
+
+static int compare_to(const void *a, const void *b)
+{
+  int fst = *(int *)a;
+  int snd = *(int *)b;
+  return fst - snd;
+}
+
+static void populate_list(void)
+{
+  for (int i = 0; i < 5; i++)
+    {
+      list_add(lst_, alloc_data(i));
+    }
+}
+
+
 void setUp(void) {
-    list = create_list();
+  lst_ = list_init(destroy_data, compare_to);
 }
 
 void tearDown(void) {
-    list_destroy(list);
+  list_destroy(&lst_);
 }
 
-void test_create_list(void) {
-    TEST_ASSERT_NOT_NULL(list);  // list is created and is not NULL
-    TEST_ASSERT_NOT_NULL(list->sentinel);  // sentinel node is created and is not NULL
-    // List is empty, so...
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, list->sentinel);  // sentinel's next is self
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->prev, list->sentinel);  // sentinel's prev is self
+
+void test_create_destroy(void)
+{
+  list_t *lst = NULL;
+  lst = list_init(destroy_data, compare_to);
+  TEST_ASSERT_FALSE(lst == NULL);
+  TEST_ASSERT_FALSE(lst->head == NULL);
+  TEST_ASSERT_TRUE(lst->size == 0);
+  TEST_ASSERT_TRUE(lst->head->data == NULL);
+  //Make sure the function pointers are pointing to the correct fuctions
+  TEST_ASSERT_TRUE(lst->destroy_data == destroy_data);
+  TEST_ASSERT_TRUE(lst->compare_to == compare_to);
+
+  //Make sure we are a circular linked list
+  TEST_ASSERT_FALSE(lst->head->next == NULL);
+  TEST_ASSERT_FALSE(lst->head->prev == NULL);
+  TEST_ASSERT_TRUE(lst->head->next == lst->head->prev);
+
+  list_destroy(&lst);
+  TEST_ASSERT_TRUE(lst == NULL);
 }
 
-void test_add1(void) {
-    int data = 42; // data to add, meaning of life
-    Node* new_node = add_data(list, &data); // get the new node from the sentinel
 
-    TEST_ASSERT_NOT_EQUAL(new_node, list->sentinel); // new node is not sentinel
-    TEST_ASSERT_EQUAL_PTR(new_node->data, &data); // new node has the data pointer we want.
+void test_add1(void)
+{
+  list_add(lst_, alloc_data(1));
+  TEST_ASSERT_TRUE(lst_->size == 1);
+  //With one node both next and prev should be equal
+  TEST_ASSERT_TRUE(lst_->head->next == lst_->head->prev);
+  //Make sure we didn't clobber our sentinel node
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->next);
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->prev);
+  TEST_ASSERT_TRUE(lst_->head->data == NULL);
+
+  //Check to make sure our data actually made it into the node
+  TEST_ASSERT_TRUE(*((int *)lst_->head->next->data) == 1);
+  TEST_ASSERT_TRUE(*((int *)lst_->head->prev->data) == 1);
 }
 
-void test_add2(void) {
-    int data1 = 1;
-    int data2 = 2;
-    Node* first_node = add_data(list, &data1);
-    Node* second_node = add_data(list, &data2);
+void test_add2(void)
+{
+  list_add(lst_, alloc_data(1));
+  TEST_ASSERT_TRUE(lst_->size == 1);
+  list_add(lst_, alloc_data(2));
+  TEST_ASSERT_TRUE(lst_->size == 2);
 
-    TEST_ASSERT_NOT_EQUAL(first_node, second_node); // different nodes
-    TEST_ASSERT_EQUAL_PTR(first_node->data, &data1); // first node has first data
-    TEST_ASSERT_EQUAL_PTR(second_node->data, &data2); // second node has second data
+  //With two nodes both next and prev should NOT be equal
+  TEST_ASSERT_FALSE(lst_->head->next == lst_->head->prev);
+  //Make sure we didn't clobber our sentinel node
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->next);
+  TEST_ASSERT_FALSE(lst_->head == lst_->head->prev);
+  TEST_ASSERT_TRUE(lst_->head->data == NULL);
+
+  //Check to make sure our next and prev have the correct data
+  TEST_ASSERT_TRUE(*((int *)lst_->head->next->data) == 2);
+  TEST_ASSERT_TRUE(*((int *)lst_->head->prev->data) == 1);
 }
 
-void test_remove_empty(void) {
-    // Empty list
-    Node* removed_node = list_remove(list, list->sentinel->next);
-    TEST_ASSERT_NULL(removed_node);  // No node should be removed (since the list is empty)
+
+void test_removeIndex0(void)
+{
+  populate_list();
+  int *rval = (int *)list_remove_index(lst_, 0);
+  TEST_ASSERT_TRUE(lst_->size == 4);
+  TEST_ASSERT_TRUE(*rval == 4);
+  free(rval);
+
+  node_t *curr = lst_->head->next;
+  //List should be 3->2->1->0
+  for (int i = 3; i >= 0; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i);
+      curr = curr->next;
+    }
+  curr = lst_->head->prev;
+  for (int i = 0; i <= 3; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i);
+      curr = curr->prev;
+    }
 }
 
-void test_remove1(void) {
-    // Add the node, so we can remove it after.
-    int data = 9;
-    Node* new_node = add_data(list, &data);
-    // Do the removal.
-    list_remove(list, new_node);
+void test_removeIndex3(void)
+{
+  populate_list();
+  int *rval = (int *)list_remove_index(lst_, 3);
+  TEST_ASSERT_TRUE(lst_->size == 4);
+  TEST_ASSERT_TRUE(*rval == 1);
+  free(rval);
 
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, list->sentinel);  // list is empty again
+  node_t *curr = lst_->head->next;
+  //List should be 4->3->2->0
+  for (int i = 3; i >= 1; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->next;
+    }
+  //Check the last one
+  TEST_ASSERT_TRUE(*((int *)curr->data) == 0);
+
+  //Set the curr back one node so we can check prev links
+  curr = curr->prev;
+  for (int i = 1; i <= 3; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->prev;
+    }
 }
 
-void test_remove2(void) {
-    int data1 = 10;
-    int data2 = 20;
-    Node* first_node = add_data(list, &data1);
-    Node* second_node = add_data(list, &data2);
 
-    // Remove the first node and test.
-    Node* removed_node = list_remove(list, first_node);
-    TEST_ASSERT_EQUAL_PTR(removed_node, first_node);  // Correct node is removed
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, second_node);  // Second node is now the first node
-    TEST_ASSERT_EQUAL_PTR(second_node->next, list->sentinel);  // Second node points to sentinel
+void test_removeIndex4(void)
+{
+  populate_list();
+  int *rval = (int *)list_remove_index(lst_, 4);
+  TEST_ASSERT_TRUE(lst_->size == 4);
+  TEST_ASSERT_TRUE(*rval == 0);
+  free(rval);
 
-    // Add a node back and remove the tail one (third, since we add to tail) this time.
-    int data3 = 30;
-    Node* third_node = add_data(list, &data3);
-    Node* removed_node = list_remove(list, third_node);
-    TEST_ASSERT_EQUAL_PTR(removed_node, third_node);
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->prev, second_node);  // Second node is now the tail node
-    TEST_ASSERT_EQUAL_PTR(second_node->next, list->sentinel);  // Second node points to sentinel
-
-    // Remove last node.
-    Node* removed_node = list_remove(list, second_node);
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, list->sentinel);  // list is empty again
+  node_t *curr = lst_->head->next;
+  //List should be 4->3->2->1
+  for (int i = 3; i >= 0; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->next;
+    }
+  curr = lst_->head->prev;
+  for (int i = 0; i <= 3; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i + 1);
+      curr = curr->prev;
+    }
 }
 
-void test_remove3(void) {
-    int data1 = 7;
-    int data2 = 8;
-    int data3 = 9;
-    Node* first_node = add_data(list, &data1);
-    Node* second_node = add_data(list, &data2);
-    Node* third_node = add_data(list, &data3);
 
-    // Remove the first node and test.
-    Node* removed_node = list_remove(list, first_node);
-    TEST_ASSERT_EQUAL_PTR(removed_node, first_node);  // Correct node is removed
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, second_node);  // Second node is now the first
-    TEST_ASSERT_EQUAL_PTR(second_node->next, third_node);  // Second node points to third node
-    TEST_ASSERT_EQUAL_PTR(third_node->next, list->sentinel);  // Third node points to sentinel
+void test_invaidIndex(void)
+{
+  populate_list();
+  void *rval = list_remove_index(lst_, 666);
+  TEST_ASSERT_TRUE(rval == NULL);
+  TEST_ASSERT_TRUE(lst_->size == 5);
 
-    // Add a node back and remove middle one this time.
-    int data4 = 14;
-    Node* fourth_node = add_data(list, &data4);
-    TEST_ASSERT_EQUAL_PTR(removed_node, third_node);
-    TEST_ASSERT_EQUAL_PTR(second_node->next, fourth_node);  // Second node points to fourth node
-    TEST_ASSERT_EQUAL_PTR(fourth_node->prev, second_node);  // Fourth node points to second
+  node_t *curr = lst_->head->next;
+  //List should be 4->3->2->1->0
+  for (int i = 4; i >= 0; i--)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) ==  i);
+      curr = curr->next;
+    }
 
-    // Add a node back and remove tail one this time.
-    int data5 = 16;
-    Node* fifth_node = add_data(list, &data5);
-    TEST_ASSERT_EQUAL_PTR(removed_node, fifth_node);
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->prev, fourth_node);  // Tail is now fourth node
-    TEST_ASSERT_EQUAL_PTR(list->sentinel, fourth_node->next); // Fourth points to sentinel
-
-    // Remove last nodes.
-    Node* removed_node = list_remove(list, second_node);
-    Node* removed_node = list_remove(list, fourth_node);
-    TEST_ASSERT_EQUAL_PTR(list->sentinel->next, list->sentinel);  // list is empty again
-
+  for (int i = 0; i >= 4; i++)
+    {
+      TEST_ASSERT_TRUE(*((int *)curr->data) == i);
+      curr = curr->prev;
+    }
 }
 
-void test_list_destroy(void) {
-    list_destroy(list);
-    // TODO - There should be no memory leaks with valgrind or makefile. Check for that.
+void test_removeAll(void)
+{
+  populate_list();
+  //List should be 4->3->2->1->0
+  for (int i = 4; i >= 0; i--)
+    {
+      int *rval = (int *)list_remove_index(lst_, 0);
+      TEST_ASSERT_TRUE(*rval == i);
+      free(rval);
+    }
+
+  //Make sure we back to default
+  TEST_ASSERT_FALSE(lst_->head->next == NULL);
+  TEST_ASSERT_FALSE(lst_->head->prev == NULL);
+  TEST_ASSERT_TRUE(lst_->head->next == lst_->head->prev);
+  TEST_ASSERT_TRUE(lst_->size == 0);
+}
+
+void test_indexOf0(void)
+{
+  populate_list();
+  //List should be 4->3->2->1->0
+  void *data = lst_->head->next->data;
+  size_t idx = list_indexof(lst_, data);
+  TEST_ASSERT_TRUE(idx == 0);
+}
+
+void test_indexOf3(void)
+{
+  populate_list();
+  //List should be 4->3->2->1->0
+  void *data = alloc_data(1);
+  size_t idx = list_indexof(lst_, data);
+  TEST_ASSERT_TRUE(idx == 3);
+  free(data);
+}
+
+void test_notInList(void)
+{
+  populate_list();
+  void *data = alloc_data(22);
+  int idx = list_indexof(lst_, data);
+  TEST_ASSERT_EQUAL_INT64(-1, idx);
+  free(data);
 }
 
 int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_create_list);
-    RUN_TEST(test_add1);
-    RUN_TEST(test_add2);
-    RUN_TEST(test_remove1);
-    RUN_TEST(test_remove2);
-    RUN_TEST(test_remove3);
-    RUN_TEST(test_list_destroy);
-    return UNITY_END();
+  UNITY_BEGIN();
+  RUN_TEST(test_create_destroy);
+  RUN_TEST(test_add1);
+  RUN_TEST(test_add2);
+  RUN_TEST(test_removeIndex0);
+  RUN_TEST(test_removeIndex3);
+  RUN_TEST(test_removeIndex4);
+  RUN_TEST(test_invaidIndex);
+  RUN_TEST(test_removeAll);
+  RUN_TEST(test_indexOf0);
+  RUN_TEST(test_indexOf3);
+  RUN_TEST(test_notInList);
+  return UNITY_END();
 }
